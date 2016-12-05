6139cfce97506f24ec04a17561333e4c76b9772d
Leaf <anqil1@andrew.cmu.edu>
2016-12-04 22:45:29 -0500

    added two robots

--- src/visualizer.py
+++ src/visualizer.py
@@ -55,11 +55,17 @@ class glWidget(QGLWidget):
     def __init__(self, parent):
         QGLWidget.__init__(self, parent)
         self.setMinimumSize(640, 480)
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        self.ugvX = 0
+        self.ugvY = 0
+        self.ugvZ = 0
 
-        self.path = [[self.x,self.y,self.z]]
+        self.uavX = 0
+        self.uavY = 0
+        self.uavZ = 20
+
+        self.ugvPath = [[self.ugvX,self.ugvY,self.ugvZ]]
+        self.uavPath = [[self.uavX,self.uavY,self.uavZ]]
+
         width,height,data = self.loadMap(GROUND_MAP)
         self.groundMap = Map(width,height,data)
 
@@ -100,10 +106,17 @@ class glWidget(QGLWidget):
 
         
         glBegin(GL_QUADS)
-        glVertex3f(self.x-0.5, self.y-0.5, self.z)
-        glVertex3f(self.x+0.5, self.y-0.5, self.z)
-        glVertex3f(self.x+0.5, self.y+0.5, self.z)
-        glVertex3f(self.x-0.5, self.y+0.5, self.z)
+        glVertex3f(self.ugvX-0.5, self.ugvY-0.5, self.ugvZ)
+        glVertex3f(self.ugvX+0.5, self.ugvY-0.5, self.ugvZ)
+        glVertex3f(self.ugvX+0.5, self.ugvY+0.5, self.ugvZ)
+        glVertex3f(self.ugvX-0.5, self.ugvY+0.5, self.ugvZ)
+        glEnd()
+
+        glBegin(GL_QUADS)
+        glVertex3f(self.uavX-0.5, self.uavY-0.5, self.uavZ)
+        glVertex3f(self.uavX+0.5, self.uavY-0.5, self.uavZ)
+        glVertex3f(self.uavX+0.5, self.uavY+0.5, self.uavZ)
+        glVertex3f(self.uavX-0.5, self.uavY+0.5, self.uavZ)
         # glVertex3f(self.x-2.6,  self.y+0.0, self.z)
         # glVertex3f(self.x-2.9,  self.y-1.2, self.z)
         glEnd()
@@ -144,9 +157,9 @@ class glWidget(QGLWidget):
         glColor3f (0.3, 0.3, 0.3);
         glBegin(GL_LINES);
         area = 50        
-        for i in range(self.groundMap.width):
+        for i in range(0,self.groundMap.width,2):
             glVertex3f(i, 0, 0); glVertex3f(i, self.groundMap.height, 0);
-        for i in range(self.groundMap.height):
+        for i in range(0,self.groundMap.height,2):
             glVertex3f(0, i, 0); glVertex3f(self.groundMap.width, i, 0);        
         
         glEnd();
@@ -160,10 +173,10 @@ class glWidget(QGLWidget):
 
         glColor4f(0.8,0.8,1,0.2)
         glBegin(GL_QUADS)
-        glVertex3f(0,0,5)
-        glVertex3f(self.groundMap.width,0,5)
-        glVertex3f(self.groundMap.width,self.groundMap.height,5)
-        glVertex3f(0,self.groundMap.height,5)
+        glVertex3f(0,0,20)
+        glVertex3f(self.groundMap.width,0,20)
+        glVertex3f(self.groundMap.width,self.groundMap.height,20)
+        glVertex3f(0,self.groundMap.height,20)
         glEnd()
 
 
@@ -171,10 +184,11 @@ class glWidget(QGLWidget):
         # show obstacles in ground map in white
         [self.DrawPatch(i,j,0.0,[1,1,1]) for i in range(self.groundMap.width) for j in range(self.groundMap.height) if self.groundMap.data[i][j]]
         # show obstacles in aerial map in blue
-        [self.DrawPatch(i,j,5.0,[0.5,0.5,1]) for i in range(self.aerialMap.width) for j in range(self.aerialMap.height) if self.aerialMap.data[i][j]]
+        [self.DrawPatch(i,j,20,[0.5,0.5,1]) for i in range(self.aerialMap.width) for j in range(self.aerialMap.height) if self.aerialMap.data[i][j]]
 
     def DrawFootprint(self):
-        [self.DrawPatch(loc[0],loc[1],loc[2],[0,1,0]) for loc in self.path]
+        [self.DrawPatch(loc[0],loc[1],loc[2],[0,1,0]) for loc in self.ugvPath]
+        [self.DrawPatch(loc[0],loc[1],loc[2],[1,0.5,0]) for loc in self.uavPath]
 
     # draw individual patches, used by drawObstacles
     def DrawPatch(self,x,y,z,color):
@@ -191,14 +205,17 @@ class glWidget(QGLWidget):
     def mousePressEvent(self, event):
         
         self.lastPos = QtCore.QPoint(event.pos())
-        self.x +=1
-        self.y +=1
-        self.path.append([self.x,self.y,self.z])
+        self.ugvX +=1
+        self.ugvY +=1
+        self.uavX +=1
+        self.uavY +=1
+        self.ugvPath.append([self.ugvX,self.ugvY,self.ugvZ])
+        self.uavPath.append([self.uavX,self.uavY,self.uavZ])
         self.updateGL()
         
 
 
-        print(self.x,self.y)
+        # print(self.x,self.y)
         
     def wheelEvent(self,event):
         d = event.delta()
@@ -210,13 +227,13 @@ class glWidget(QGLWidget):
             self.close()
         else:
             if event.key() == QtCore.Qt.Key_Up:
-                self.cameraY -= 2
-            if event.key() == QtCore.Qt.Key_Down:
                 self.cameraY += 2
+            if event.key() == QtCore.Qt.Key_Down:
+                self.cameraY -= 2
             if event.key() == QtCore.Qt.Key_Left:
-                self.cameraX += 2
-            if event.key() == QtCore.Qt.Key_Right:
                 self.cameraX -= 2
+            if event.key() == QtCore.Qt.Key_Right:
+                self.cameraX += 2
 
             if event.key() == QtCore.Qt.Key_W:
                 self.rotX -= 2
